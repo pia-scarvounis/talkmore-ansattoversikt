3997ec98e0e9281001312503f43e2c944025133e
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-04-03 16:44:11 +0200

    lagt til en container for bakgrunfargen som i prototypen

--- src/components/ProfileCards.jsx
+++ src/components/ProfileCards.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "../styles/global.css"
 //importere css for profilkort
+
+import "../styles/profilecards.css"
+
 import profileImage from "../assets/images/default-img.png"
 
 const ProfileCards = () => {
@@ -8,13 +11,12 @@ const ProfileCards = () => {
     //Sette inn betingelser for farge på kortene om det blir rosa/blå bakgrunn
   return (
     <div className='profile-card'>
-      <h1> HEI DETTE ER TEST Test</h1>
-
+        
         <h3>Fornavn Etternavn</h3>
         <p>Ansattnr(Talkmore) </p>
         <p>Ansattnr(Telenor)</p>
         
-        //Profilbilde
+        {/**Profilbilde*/}
         <div className='profile-img-container'>
             <img 
                 src={profileImage} 
@@ -23,7 +25,7 @@ const ProfileCards = () => {
             />
         </div>
 
-        //Ansatt info
+        {/**Ansatt/jobb informasjon*/}
         <p><span className='label-info'>Stilling :</span>ansatt.stilling</p>
         <p><span className='label-info'>Fast/Innleid :</span>ansatt.Fast/Innleid</p>
         <p><span className='label-info'>Stilling% :</span>ansatt.stilling%</p>        
