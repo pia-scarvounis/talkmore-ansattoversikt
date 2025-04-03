a9450c9c27d6d8beaae573b22b64cadac189fe5d
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-04-03 15:35:20 +0200

    Lagt til struktur for profilkortene

--- src/components/ProfileCards.jsx
+++ src/components/ProfileCards.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import "../styles/global.css"
+//importere css for profilkort
+import profileImage from "../assets/images/default-img.png"
 
 const ProfileCards = () => {
+    //Hente inn en ansatt
+    //Sette inn betingelser for farge på kortene om det blir rosa/blå bakgrunn
   return (
-    <div className={cardClass}>
+    <div>
       <h1> HEI DETTE ER TEST Test</h1>
 
+        <p>Fornavn Etternavn</p>
+        <p>Ansattnr(Talkmore) </p>
+        <p>Ansattnr(Telenor)</p>
+        
+        //Profilbilde
+        <div>
+            <img src={profileImage} alt="Profilbilde Ansatt"></img>
+        </div>
+        //Ansatt info
+        <p><span>Stilling :</span>ansatt.stilling</p>
+        <p><span>Fast/Innleid :</span>ansatt.Fast/Innleid</p>
+        <p><span>Stilling% :</span>ansatt.stilling%</p>        
+
+
     </div>
-  )
+  );
 }
 
 export default ProfileCards
