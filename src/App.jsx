2e283f0c8af2d2584f004caecae8103907ce81a6
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-04-03 14:41:01 +0200

    lagt til fil pathen for profilkort i ruteren midlertidig

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import DashBoardAdmin from "./pages/DashBoardAdmin";
+import ProfileCards from "./components/ProfileCards";
 import "./styles/global.css";
 
 function App() {
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<DashBoardAdmin />} />
+        <Route path="/profilecards" element={<ProfileCards />} />
+
       </Routes>
     </Router>
     
