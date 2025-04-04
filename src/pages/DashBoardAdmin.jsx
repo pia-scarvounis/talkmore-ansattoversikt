c8dbde6a9f11a78a41e5c1f69f011d5c5e4dea32
zaazi99 <zainab_aziz@hotmail.com>
2025-04-04 18:57:16 +0200

    la inn små endringer i dashboard

--- src/pages/DashBoardAdmin.jsx
+++ src/pages/DashBoardAdmin.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import NavAdmin from "../components/navigation/NavAdmin";
+import StatBox from "../components/StatBox";
+import "../styles/dashboard.css";
 
 const DashBoardAdmin = () => {
   return (
-    <div>
+    <div className="dashboard-layout">
       <NavAdmin />
 
-      <div>
-        <h1>Admin Dashboard</h1>
-        {/* Her kommer resten*/}
+      <div className="dashboard-wrapper">
+        <h1>Dagsoversikt</h1>
+
+        <div className="dashboard-grid">
+          <StatBox title="TEAMLEDERE" value={6} />
+          <StatBox title="KS ADMIN" value={7} />
+          <StatBox title="KUNDEANSVARLIG" value={40} />
+          <StatBox title="TELENORANSATTE" value={35} />
+          <StatBox title="INNLEID" value={5} />
+          <StatBox title="HELTID" value={35} />
+          <StatBox title="DELTID" value={5} />
+          <StatBox
+            title="FULLTIDSEKVIVALENTER"
+            value="37,5"
+            unit="FTE (KA)"
+            highlight
+          />
+        </div>
       </div>
     </div>
   );
