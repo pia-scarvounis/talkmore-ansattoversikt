1cd33854870b2b37b9ec7a6920f2d06fed5cff51
Pia Christina Scarvounis <pia.scarvounis@gmail.com>
2025-04-04 18:34:07 +0200

    fikset merge konflikt i app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import DashBoardAdmin from "./pages/DashBoardAdmin";
+import ProfileCards from "./components/Employee/ProfileCards";
+import ProfileCardList from "./components/ProfileCardLists";
 import "./styles/global.css";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
 
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<DashBoardAdmin />} />
+        <Route path="/profilecards" element={<ProfileCards />} />
+        <Route path="/profilecardslist" element={<ProfileCardLists />} />
       </Routes>
     </Router>
     
