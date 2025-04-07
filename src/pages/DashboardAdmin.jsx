80490adc12d6cc5063e8436062613da57c4f1a0f
zaazi99 <zainab_aziz@hotmail.com>
2025-04-07 18:50:11 +0200

    la inn noen endringer i dashboard

--- src/pages/DashboardAdmin.jsx
+++ src/pages/DashboardAdmin.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavAdmin from "../components/navigation/NavAdmin";
-import StatBox from "../components/StatBox";
+import StatBox from "../components/Dashboard/StatBox";
+import EventBox from "../components/Dashboard/EventBox";
 import "../styles/dashboard.css";
 
 const DashboardAdmin = () => {
@@ -9,22 +10,32 @@ const DashboardAdmin = () => {
       <NavAdmin />
 
       <div className="dashboard-wrapper">
-        <h1>Dagsoversikt</h1>
+        <div className="left-column">
+          <h1>Dagsoversikt</h1>
 
-        <div className="dashboard-grid">
-          <StatBox title="TEAMLEDERE" value={6} />
-          <StatBox title="KS ADMIN" value={7} />
-          <StatBox title="KUNDEANSVARLIG" value={40} />
-          <StatBox title="TELENORANSATTE" value={35} />
-          <StatBox title="INNLEID" value={5} />
-          <StatBox title="HELTID" value={35} />
-          <StatBox title="DELTID" value={5} />
-          <StatBox
-            title="FULLTIDSEKVIVALENTER"
-            value="37,5"
-            unit="FTE (KA)"
-            highlight
-          />
+          <div className="dashboard-grid">
+            <StatBox title="TEAMLEDERE" value={6} />
+            <StatBox title="KS ADMIN" value={7} />
+            <StatBox title="KUNDEANSVARLIG" value={40} />
+
+            <StatBox title="TELENORANSATTE" value={35} />
+            <StatBox title="INNLEID" value={5} />
+
+            <StatBox title="HELTID" value={35} />
+            <StatBox title="DELTID" value={5} />
+
+            <StatBox
+              title="FULLTIDSEKVIVALENTER"
+              value="37,5"
+              unit="FTE (KA)"
+              highlight
+              className="fte"
+            />
+          </div>
+        </div>
+
+        <div className="right-column">
+          <EventBox />
         </div>
       </div>
     </div>
