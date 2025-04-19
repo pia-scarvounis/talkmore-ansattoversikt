b402644d6049c8fba130d9633ccee726756e87cc
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-04-19 12:50:17 +0200

    fikset en del logikk for random instert til databasen

--- src/backend/db_routes/users_routes.js
+++ src/backend/db_routes/users_routes.js
@@ -2,9 +2,6 @@ import { application, json, Router } from "express";
 import dBpool from "../config/db.js";
 import dotenv from "dotenv";
 import pool from "../config/db.js";
-const [dbResult] = await pool.query("SELECT DATABASE() AS db");
-console.log("🧠 Koden kjører mot databasen:", dbResult[0].db);
-
 import axios from "axios";
 import {getOAuthToken} from '../apiGenesysAuth/authTokenGenesys.js'
 //vi skal importere en autentisering middleware for alle brukere av vårt verktøy
@@ -15,6 +12,9 @@ dotenv.config();
 
 const router = Router();
 
+const [dbResult] = await pool.query("SELECT DATABASE() AS db");
+console.log("🧠 Koden kjører mot databasen:", dbResult[0].db);
+
  //funksjon random id fra en tabell (fk)
 async function getRandomId(idField, table){
     const [rows] = await pool.query(`SELECT ${idField} FROM ${table}`);
@@ -22,11 +22,10 @@ async function getRandomId(idField, table){
     const randomRow = rows[Math.floor(Math.random()* rows.length)];
     return randomRow[idField];
 }
-const formOptions = ['Fast', 'Innleid'];
 
+const formOptions = ['Fast', 'Innleid'];
 //funksjon sjekk om employee er Admin,Teamleder eller KA, Admin+Teamledere skal ha deafult 100% stilling
 async function getRandomWorkPosistionTitle(){
-
     const [rows] = await pool.query(`SELECT workPosistion_id, posistion_title FROM workPosistion `);
     console.log('Rows from database:', rows);
     if(rows.length === 0) throw new Error('Ingen stillinger i databasen');
@@ -68,7 +67,7 @@ router.get('/', async (req, res) => {
             const randomBirthday = () => new Date(1980 + Math.random() * 21, Math.random() * 12, Math.random() * 28 | 0)
                     .toISOString().split('T')[0];
             const birthdate = randomBirthday();
-            const randomFormOfEmployement = formOptions[Math.floor(Math.random()*formOptions.length)];
+            /*const randomFormOfEmployement = formOptions[Math.floor(Math.random()*formOptions.length)];*/
             const randomStartDate = () => new Date(2010, 0, 1 + Math.random() * ((Date.now() - new Date(2010, 0, 1)) / 86400000) | 0).toISOString().split('T')[0];
             const start_date = randomStartDate();
             const randomEndDate = null;
@@ -78,14 +77,35 @@ router.get('/', async (req, res) => {
 
             //gyldig id fra tabeller FK foreign key (parametere til getRandomId(idField, table))
             const team_id = await getRandomId('team_id','team');
-
+            // Hent team_name basert på team_id
+            const [teamRows] = await pool.query(
+                'SELECT team_name FROM team WHERE team_id = ?',
+                [team_id]
+            );
+            const team_name = teamRows[0]?.team_name || 'Ukjent team';
+  
             const workPos =  await getRandomWorkPosistionTitle();
             const workPosistion_id = workPos.workPosistion_id;
-            const title = workPos.posistion_title;
+            const workPos_title = workPos.posistion_title;
 
             let employee_percentages = 100;
+            let form_of_employeement = 'Fast';
+            //Logikk til å sette fast på Admin og Teamleder mens kundeagenter skal få random
+            //form_of_employeement
+            if (workPos_title.toLowerCase() === "kundeagent") {
+                employee_percentages = Math.floor(Math.random() * 91) + 10; // 10–100
+                form_of_employeement =
+                  formOptions[Math.floor(Math.random() * formOptions.length)];
+              } else if (
+                workPos_title.toLowerCase() === "admin" ||
+                workPos_title.toLowerCase() === "teamleder"
+              ) {
+                employee_percentages = 100;
+                form_of_employeement = "Fast";
+              }
 
-            if(title === 'kundeAgent'){
+
+            if(workPos_title === 'kundeAgent'){
                 employee_percentages = Math.floor(Math.random() * 51) + 50;
             }
 
@@ -95,8 +115,9 @@ router.get('/', async (req, res) => {
                 `INSERT INTO employee(
                     employee_name, epost, phoneNr, birthdate, image_url, start_date, end_date,
                     form_of_employeement, employeeNr_Talkmore, employeeNr_Telenor, 
-                    employee_percentages, is_test, team_id, workPosistion_id
-                ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,[
+                    employee_percentages, is_test, team_id, workPosistion_id, 
+                    team_name, workPosistion_title
+                ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,[
                     employee.name,
                     employee.email,
                     randomPhone,
@@ -104,32 +125,38 @@ router.get('/', async (req, res) => {
                     null,
                     start_date,
                     end_date,
-                    randomFormOfEmployement,
+                    form_of_employeement,
                     employeNr_TM,
                     employeNr_TN,
                     employee_percentages,
                     true,
                     team_id,
-                    workPosistion_id
+                    workPosistion_id,
+                    team_name,
+                    workPos_title
+                    
                 ]
             );
 
             return{
                 ...employee,
                 dbId: result.id,
+                name:employee.name,
+                epost: employee.email,
                 phoneNr: randomPhone,
-                birthdate: randomBirthday,
+                birthdate: birthdate,
                 image_url: null,
-                start_date: randomStartDate,
-                end_date: randomEndDate,
-                form_of_employeement: randomFormOfEmployement,
+                start_date: start_date,
+                end_date: end_date,
+                form_of_employeement: form_of_employeement,
                 employeeNr_Talkmore: employeNr_TM,
                 employeeNr_Telenor: employeNr_TN,
                 employee_percentages:employee_percentages,
                 is_test: true,
                 team_id: team_id,
-                workPosistion_id: workPosistion_id
-
+                team_name: team_name,
+                workPosistion_id: workPosistion_id,
+                workPos_title: workPos_title
             };
         })
     );
