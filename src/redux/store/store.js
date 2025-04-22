3eceed228e849fe6c10d67f9d8f14615e4778667
zaazi99 <zainab_aziz@hotmail.com>
2025-04-22 17:47:10 +0200

    la inn notesreducer i store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dateReducer from "../slices/dateSlice";
-import employeeReducer from '../slices/employeeSlice';
+import employeeReducer from "../slices/employeeSlice";
+import noteReducer from "../slices/noteSlice";
 
 export const store = configureStore({
   reducer: {
     date: dateReducer,
     employees: employeeReducer,
+    notes: noteReducer,
   },
 });
