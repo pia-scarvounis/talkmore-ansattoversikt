79c6d288a5865237006a76d3ea8eac2c6a2f991b
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-04-24 19:51:31 +0200

    endret til å også hente navnet på admin som har endret ansatt info

--- src/backend/db_routes/UsersRoutes/getEmpHistory_router.js
+++ src/backend/db_routes/UsersRoutes/getEmpHistory_router.js
@@ -3,5 +3,26 @@ import pool from "../../config/db.js";
 
 //hente historikken til den valgte ansatte
 router.get('/:id', async (req, res) =>{
-    
+    const {id} = req.params;
+
+    try{
+        //Henter brukerna
+        const [history] =await pool.query(`
+            SELECT
+                cl.*,
+                u.username AS endret_av,
+                e.employee_name AS endret_av_navn
+            FROM changelog cl
+            JOIN userOfTool u ON cl.admin_id = u.user_id
+            JOIN employee e ON u.employee_id = e.employee_id
+            WHERE cl.employee_id = ?
+            ORDER BY cl.change_date DESC
+        `,[id]);
+
+        res.status(200).json(history);
+
+    }catch(err){
+        console.error('Feil ved henting av historikk:',err);
+        res.status(500).json({error: 'kunne ikke hente historikk'});
+    }
 })
