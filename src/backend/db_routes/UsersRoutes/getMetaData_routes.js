de64c39cd866ba77791c066752843e1898554bd7
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-04-28 17:28:03 +0200

    setter opp rutere for å hente team

---
+++ src/backend/db_routes/UsersRoutes/getMetaData_routes.js
@@ -0,0 +1,21 @@
+import {Router} from 'express';
+import {pool} from '../../config/db.js';
+
+const router = Router();
+
+//Hente alle avdelinger
+router('/teams', async (req, res) =>{
+    try{
+
+    }catch{
+
+    }
+})
+
+
+//Hente alle Team
+
+
+//Hente alle stillinger
+
+export default router;
