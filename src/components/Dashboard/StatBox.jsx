6278895a964d0c2f265990235fdbdaa59496bc13
Pia Christina Scarvounis <pia.scarvounis@gmail.com>
2025-05-03 16:33:26 +0200

    dashboardbokser har fått cursor:pointer, fte ikke.

--- src/components/Dashboard/StatBox.jsx
+++ src/components/Dashboard/StatBox.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../../styles/dashboard.css";
 
 const StatBox = ({ title, value, unit, highlight, className, icon, onClick }) => {
+  
   return (
     <div
       className={`stat-box ${highlight ? "highlight" : ""} ${className || ""}`}onClick={onClick}
