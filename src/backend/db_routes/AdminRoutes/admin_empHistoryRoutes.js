a24878d74dedaf3a9fdf02eee6960d7154b320bc
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-05-03 13:57:28 +0200

    fikset put historikk til patch

--- src/backend/db_routes/AdminRoutes/admin_empHistoryRoutes.js
+++ src/backend/db_routes/AdminRoutes/admin_empHistoryRoutes.js
@@ -6,59 +6,42 @@ import pool from "../../config/db.js";
 const router = Router();
 dotenv.config();
 
+//Post histotikk skjer i employee put rute når man endrer en ansatt- da legges det til i historikken
+
 //Endre historikk med changeLog id
-router.put('/history/:changleLog_id', async (req, res) => {
+router.patch('/history/:changleLog_id', async (req, res) => {
 
     const {changeLog_id} = req.params;
+    const fields = [];
+    const values = [];
 
-    const {
-        department_id,
-        team_id,
-        workPosistion_id,
-        form_of_employeement,
-        employee_percentages,
-        start_date,
-        end_date,
-        leave_percentage,
-        leave_start_date,
-        leave_end_date
-    } = req.body;
+    const allowedFields = 
+    ['department_id', 'team_id', 'workPosistion_id'
+    , 'form_of_employeement', 'employee_percentages',  
+    'start_date', 'end_date',
+    'leave_percentage', 'leave_start_date', 'leave_end_date'
+    ];
 
-    try{
-        const [result] = await pool.query(`
-            UPDATE changeLog
-            SET
-                department_id = ?,
-                team_id = ?,
-                workPosistion_id = ?,
-                form_of_employeement = ?,
-                employee_percentages = ?,
-                start_date = ?,
-                end_date = ?,
-                leave_percentage = ?,
-                leave_start_date = ?,
-                leave_end_date = ?
-            WHERE changeLog_id = ?
-        `, [
-            department_id,
-            team_id,
-            workPosistion_id,
-            form_of_employeement,
-            employee_percentages,
-            start_date,
-            end_date,
-            leave_percentage,
-            leave_start_date,
-            leave_end_date,
-            changeLog_id
-        ]);
-
-        if(result.affectedRows === 0){
-            return res.status(404).json({error: 'Fant ikke historikk post med angitt changeLog id'});
+    for(const field of allowedFields){
+        if(field in req.body){
+            //setter inn feltene i []
+            fields.push(`${field} = ?`);
+            //henter inn verdien fra body og setter inn felt
+            values.push(req.body[field]);
         }
+    }
+    if(fields.length === 0){
+        return res.status(400).json({error: 'ingen gyldige felter å oppdatere'});
+    }
+     try{
+
+        await pool.query(
+            `UPDATE changeLog SET ${(fields.join(', '))} WHERE changeLog_id`,
+            [...values, id]
+        );
         res.status(200).json({message: 'Historikk oppdatert'});
 
-    }catch(err){
+     }catch(err){
         console.error('Feil ved oppdatering av historikk', err);
         res.status(500).json({error:'Kunne ikke oppdatere historikk'});
     }
@@ -66,6 +49,5 @@ router.put('/history/:changleLog_id', async (req, res) => {
 });
 
 //Delete ruter for å slette felt i historikken??? man kan kun endre bør ikke slette felt i historikk
-
 
 export default router;
