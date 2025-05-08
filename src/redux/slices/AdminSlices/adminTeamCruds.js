25b21fa63ff327a7afea0f638f2d45cfd8746ff0
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-05-08 13:33:21 +0200

    lagt til resten av håndtering av fetcher med addMatcher for team cruds

--- src/redux/slices/AdminSlices/adminTeamCruds.js
+++ src/redux/slices/AdminSlices/adminTeamCruds.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import api from '../../../backend/apiToken/axiosInstance.js';
 //importerer fetchMetadata da get teams ligger i en annen slice, den skal helst ikke det men når vi skal hente ut mye på en gang
 //har vi laget egen get for avdeling, teams, stillinger og lisenser i en slice
-//Så vi må hente inn metadata og oppdatere arrayet for teams i metadata slice
+//Så vi må hente inn metadata og oppdatere arrayet i denne slicen
 
 import { fetchMetaData } from '../metaDataCrudsSlice.js';
 
@@ -34,3 +34,40 @@ export const deleteTeam = createAsyncThunk(
 );
 
 //SLICE
+// Det blir benyttet en addMatcher istedenfor addcase for mindre kode,
+// istedenfor å lage addcaser for create, update og delete får de felles caser med matcher i pending/rejected/success
+//Gpt tips
+
+const  teamCrudSlice = createSlice({
+    name: 'teamCruds',
+    initialState: {
+        loading: false,
+        error: null
+    },
+    reducers:{},
+    extraReducers: (builder) => {
+        builder
+            .addMatcher(
+                (action) => action.type.startsWith('team/') && action.type.endsWith('/pending'),
+                (state) => {
+                    state.loading = true;
+                    state.error = null;
+                }
+            )
+            .addMatcher(
+                (action) => action.type.startsWith('team/') && action.type.endsWith('/fulfilled'),
+                (state) => {
+                    state.loading = false;
+                }
+            )
+            addMatcher(
+                (action) => action.type.startsWith('team/') && action.type.endsWith('/rejected'),
+                (state, action) => {
+                    state.loading = false;
+                    state.error = action.payload || 'Noe gikk galt med team handling/action';
+                }
+            );
+    }
+});
+
+export default teamCrudSlice.reducer;
