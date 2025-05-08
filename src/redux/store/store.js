691bd7fe65767198edab765604ad13e849b8d7bc
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-05-08 13:37:02 +0200

    la admin team cruds i redux store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -8,6 +8,7 @@ import employeeHistoryReducer from '../slices/historySlice';
 import updateEmployeeReducer from '../slices/AdminSlices/adminEmpl_CrudsSlice';
 import adminUpdateHistoryReducer from '../slices/AdminSlices/adminHistoryCrudSlice';
 import metaDataReducer from '../slices/metaDataCrudsSlice';
+import adminTeamCrudsReducer from '../slices/AdminSlices/adminTeamCruds';
 import authReducer from '../slices/authLoginSlice/loginSlice';
 
 export const store = configureStore({
@@ -21,6 +22,7 @@ export const store = configureStore({
     availableEmployees: availableEmployeesReducer,
     changeLog: adminUpdateHistoryReducer,
     metaData: metaDataReducer,
+    teamCruds: adminTeamCrudsReducer,
     auth: authReducer
   },
 });
