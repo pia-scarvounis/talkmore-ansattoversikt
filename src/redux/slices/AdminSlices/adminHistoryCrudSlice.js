1b15458dabc7e4fdcc94e3063750828b50c281df
sarah <sarahsheikh@sarahs-MacBook-Pro.local>
2025-05-11 17:23:28 +0200

    lagt til (api) fetch får token i header i patch ruteren for history

--- src/redux/slices/AdminSlices/adminHistoryCrudSlice.js
+++ src/redux/slices/AdminSlices/adminHistoryCrudSlice.js
@@ -11,7 +11,7 @@ export const updateChangeLog = createAsyncThunk(
     'changeLog/updateChangeLog',
     async({ changleLogId, updatedFields }, {rejectedWithValue}) => {
         try{
-            const response = await axios.patch(`http://localhost:3000/api/history/${changleLogId}`,
+            const response = await api.patch(`http://localhost:3000/api/history/${changleLogId}`,
             updatedFields,
             {
                 headers: {
