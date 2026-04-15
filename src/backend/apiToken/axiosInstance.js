1475dd275dc3fa3182aa5a678f896add523589f0
Pia Scarvounis <pia.scarvounis@gmail.com>
2026-04-15 19:22:12 +0200

    endret baseURL

--- src/backend/apiToken/axiosInstance.js
+++ src/backend/apiToken/axiosInstance.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: '/api'
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true
 });
 
 api.interceptors.request.use(config => {
